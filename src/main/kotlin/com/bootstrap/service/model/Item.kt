97ce64cae9d4cd6524c557037ba0e6ba7df43938
Amith Nair <amithnair91@gmail.com>
2018-09-16 16:06:54 +0530

    add collection name

--- src/main/kotlin/com/bootstrap/service/model/Item.kt
+++ src/main/kotlin/com/bootstrap/service/model/Item.kt
@@ -3,5 +3,5 @@ package com.bootstrap.service.model
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document
+@Document(collection = "item")
 data class Item(val name: String, val value: String, @Id val id: String? = null)
