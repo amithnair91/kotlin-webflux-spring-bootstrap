a642092749e849a348e3054ef185cf8f217426c6
Amith Nair <amithnair91@gmail.com>
2018-09-16 10:47:45 +0530

    first commit

---
+++ src/main/kotlin/com/bootstrap/bootstrapservice/BootstrapServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.bootstrap.bootstrapservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BootstrapServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<BootstrapServiceApplication>(*args)
+}
