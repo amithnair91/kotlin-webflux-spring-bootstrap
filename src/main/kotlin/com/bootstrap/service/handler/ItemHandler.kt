7f5752d666c41a28bcfe645c709ceaced8c85a6f
Amith Nair <amithnair91@gmail.com>
2018-09-18 09:08:41 +0530

    add logging

--- src/main/kotlin/com/bootstrap/service/handler/ItemHandler.kt
+++ src/main/kotlin/com/bootstrap/service/handler/ItemHandler.kt
@@ -2,20 +2,28 @@ package com.bootstrap.service.handler
 
 import com.bootstrap.service.model.Item
 import com.bootstrap.service.repository.ItemRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
+import reactor.core.publisher.Mono
 
 
 @Component
 class ItemHandler(@Autowired val repository: ItemRepository) {
 
+    var logger = LoggerFactory.getLogger(ItemHandler::class.java)
+
     private fun json() = ok().contentType(APPLICATION_JSON_UTF8)
 
-    fun findAll(request: ServerRequest) = json().body(repository.findAll())
+    fun findAll(request: ServerRequest): Mono<ServerResponse> {
+        logger.info("finding all")
+       return  json().body(repository.findAll())
+    }
 
     fun findOne(request: ServerRequest) = json().body(repository.findByName(request.pathVariable("name")))
 
